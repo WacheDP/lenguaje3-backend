c2f1ab9aad54499e200e1e744dbbfaf28408c67e
Daniel Parra <wacheparra21@gmail.com>
2025-06-03 23:23:02 -0400

    Backend terminado

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\MercanciaControlador;
 use App\Http\Controllers\UsuariosControlador;
+use App\Http\Controllers\VentasControlador;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ Route::get("/clientes/{pagina}", [UsuariosControlador::class, "Listado_Cliente"]
 Route::get("/cliente/{usuario}", [UsuariosControlador::class, "Informacion"]);
 Route::post("/mercancia", [MercanciaControlador::class, "Registrar"]);
 Route::get("/mercancia/{pagina}", [MercanciaControlador::class, "Lista"]);
+Route::post("/ventas", [VentasControlador::class, "Comprar"]);
+Route::get("/ventas/{pagina}", [VentasControlador::class, "Lista"]);
